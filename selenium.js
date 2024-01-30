0e17559950ded763879756cd50be51a1b095d97c
spde3289 <spde3289@naver.com>
2024-01-31 02:56:50 +0900

    selenium 크롤링

--- selenium.js
+++ selenium.js
@@ -1,19 +1,21 @@
 const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 
-let driver = new webdriver.Builder()
-  .forBrowser(webdriver.Browser.CHROME)
-  .setChromeOptions(/* ... */)
-  .setFirefoxOptions(/* ... */)
-  .build();
 
 (async function example() {
+  const list = ["환산", "헥사환산"];
   let driver = await new Builder().forBrowser(Browser.CHROME).build();
   try {
-    await driver.get("https://www.google.com/ncr");
-    console.log(driver)
-    await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-    await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+    await driver.get(
+      "https://maplescouter.com/info?name=%EA%B3%B5%EB%8F%8C%EC%A7%80%EB%A0%81mk1&date=2024-01-24"
+    );
+    let s = await driver.findElement(
+      By.className("flex gap-[20px] mo:flex-col")
+    );
+    let ss = await s.getText();
+
+    console.log(ss.split("\n").map((el) => list.includes(el)));
+    console.log(ss.split("\n").find( el => list.includes(el)));
   } finally {
     await driver.quit();
   }
